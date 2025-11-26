2267ea9ab9e46a210a50c57727cb0aa49aa1e333
romaazzam812-web <romaazzam812@gmail.com>
2025-11-26 22:55:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const loginBtn = document.querySelector("#btn")
+const Enter = document.querySelector("#Enter")
+const result= document.querySelector("#result")
+
+loginBtn .addEventListener ("calculate", function() {
+
+    const Enter = Enter.value
+
+    if ("Enter === Enter your degree"){
+
+        result.textContent = "Your degree is 82, then your grade is b."
+    }
+} )
